c73fd3199c3793f2c2dcaf1a58d1dca4f87eddb2
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-20 15:43:31 -0300

    Crear config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# ⚙️ CONFIGURACIÓN GENERAL
+# ⏱️ Duración de cada fragmento en segundos
+CLIP_DURATION = 60  
+
+# 🔁 Reintentos si falla el envío
+MAX_RETRIES = 3
+# ⏳ Tiempo de espera máximo por publicación
+TIMEOUT_SEND = 300
+
+# 📂 Rutas de trabajo
+TEMP_FOLDER = "videos/output"
+INPUT_FOLDER = "videos/input"
+
+# 📱 Datos de conexión WhatsApp
+CODIGO_SESION = "" # Se ingresa desde la interfaz
